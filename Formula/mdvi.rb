355375fb40e55cbc62d35aaea911dc04f5178112
Todd Fisher <todd.fisher@gmail.com>
2026-02-10 20:07:30 -0500

    mdvi 0.5.0

--- Formula/mdvi.rb
+++ Formula/mdvi.rb
@@ -1,8 +1,8 @@
 class Mdvi < Formula
   desc "Terminal markdown viewer with Vim-style navigation"
   homepage "https://github.com/taf2/mdvi"
-  url "https://github.com/taf2/mdvi/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "f075d162b4c248b08e60692d623217e3d1ad2b219bbcf74c0bba13214030211d"
+  url "https://github.com/taf2/mdvi/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "67d7771e91d1958eadf3335e97eb27ac39dc0d9c1ffc44e8cd152f4cd9b3c388"
   license "MIT"
 
   depends_on "rust" => :build
